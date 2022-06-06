48861a0fedd3013f597c616eb039f2779dcec5e6
Toby <tobywas@gmail.com>
2022-06-05 20:46:59 -0400

    img fix

--- parts/SideBar.js
+++ parts/SideBar.js
@@ -10,7 +10,7 @@ export default props => {
     <Menu right {...props} className="menu" >
       <a href='#'>
           <span className="profilePic">
-              <Image src='/assets/cattoblush.png' alt="Profile"/>
+              <Image src={cattoblush} alt="Profile"/>
           </span>
       </a>
         <a className="menu-item" href="#">
